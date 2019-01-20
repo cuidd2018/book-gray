9cf6e893e51e1adc9fc3ca3247f900f28a14ce17
haixiang_wang <haixiang_wang@dingyuegroup.cn?>
2019-01-20 14:53:27 +0800

    修改MQ为广播模式

--- book-gray-server/src/main/java/cn/dingyuegroup/gray/server/mq/config/ProducerConfig.java
+++ book-gray-server/src/main/java/cn/dingyuegroup/gray/server/mq/config/ProducerConfig.java
@@ -4,6 +4,7 @@ import cn.dingyuegroup.gray.server.config.GrayMQMarkerConfiguration;
 import com.aliyun.openservices.ons.api.ONSFactory;
 import com.aliyun.openservices.ons.api.Producer;
 import com.aliyun.openservices.ons.api.PropertyKeyConst;
+import com.aliyun.openservices.ons.api.PropertyValueConst;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
@@ -43,6 +44,7 @@ public class ProducerConfig {
         properties.setProperty(PropertyKeyConst.SecretKey, secretKey);
         properties.setProperty(PropertyKeyConst.SendMsgTimeoutMillis, sendMsgTimeoutMillis);
         properties.setProperty(PropertyKeyConst.ONSAddr, onsAddr);
+        properties.setProperty(PropertyKeyConst.MessageModel, PropertyValueConst.BROADCASTING);
 
         Producer producer = ONSFactory.createProducer(properties);
         // 在发送消息前，必须调用 start 方法来启动 Producer，只需调用一次即可
